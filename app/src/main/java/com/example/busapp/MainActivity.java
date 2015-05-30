6a7a3937108c60fd21713a90fa9eb4880b78b648
naddy095 <nadeemmohammad9@gmail.com>
2015-05-30 16:24:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/busapp/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.busapp;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnLongClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+	LinearLayout datePicker;
+    LinearLayout shortJourney;
+    LinearLayout longJourney;
+	final Context context=this;
+    LinearLayout cityFromSelection;
+    String journeyType = "";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		android.support.v7.app.ActionBar ab =getSupportActionBar();
+		ab.setDisplayShowHomeEnabled(true);
+		ab.setIcon(R.drawable.ic_launcher);
+		setContentView(R.layout.activity_main);
+        cityFromSelection = (LinearLayout)findViewById(R.id.pickAreaLL);
+        cityFromSelection.setOnClickListener(this);
+		datePicker = (LinearLayout)findViewById(R.id.datePickerLayout);
+
+        //journey tabs
+        shortJourney = (LinearLayout)findViewById(R.id.shortJourneyLL);
+        longJourney = (LinearLayout)findViewById(R.id.longJourneyLL);
+        shortJourney.setOnClickListener(this);
+        longJourney.setOnClickListener(this);
+        Intent intent = getIntent();
+        String selectedArea = intent.getStringExtra("area");
+        if(selectedArea !=null && !selectedArea.isEmpty()){
+            ((TextView)findViewById(R.id.areaText)).setText(selectedArea);
+        }
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+		//		MenuInflater inflater = getMenuInflater();
+		//        inflater.inflate(R.menu.activity_main_actions, menu);
+		//
+		//        return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+    @Override
+    public void onClick(View view) {
+        TextView shortTv;
+        TextView longTv;
+        LinearLayout LL;
+        switch(view.getId())
+        {
+            case R.id.pickAreaLL :
+                Intent intent = new Intent(context,PickArea.class);
+                startActivity(intent);
+                break;
+            case R.id.shortJourneyLL :
+                shortTv = (TextView)findViewById(R.id.shortJourneyTV);
+                longTv = (TextView)findViewById(R.id.longJourneyTV);
+                LL = (LinearLayout)findViewById(R.id.longJourneyLL);
+                LL.setBackgroundColor(Color.WHITE);
+                longTv.setTextColor(Color.GRAY);
+                shortTv.setTextColor(Color.WHITE);
+                view.setBackgroundColor(Color.GRAY);
+                Toast.makeText(context,"shortJourneyLL",Toast.LENGTH_SHORT).show();
+                journeyType = "shortjourney";
+                break;
+            case R.id.longJourneyLL :
+                shortTv = (TextView)findViewById(R.id.shortJourneyTV);
+                longTv = (TextView)findViewById(R.id.longJourneyTV);
+                LL = (LinearLayout)findViewById(R.id.shortJourneyLL);
+                view.setBackgroundColor(Color.GRAY);
+                shortTv.setTextColor(Color.GRAY);
+                longTv.setTextColor(Color.WHITE);
+                LL.setBackgroundColor(Color.WHITE);
+                journeyType = "longjourney";
+                Toast.makeText(context,"longJourneyLL",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+   /* @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch(view.getId())
+        {
+            case R.id.fromLayout :
+                Toast.makeText(context,"From Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.toLayout :
+                Toast.makeText(context,"To Clicked",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }*/
+}
---
+++ app/src/main/java/com/example/busapp/PickArea.java
@@ -0,0 +1,86 @@
+package com.example.busapp;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class PickArea extends ListActivity {
+
+    final Context context=this;
+    static final String[] pickArea = new String[] {  "Abbigere", "Aecs Layout", "Airport Area", "Airport Road", "Akshaya Nagar", "Anekal",
+            "Anjanapura", "Attibele", "Bagalur", "Banashankari", "Banaswadi", "Bannerghatta Road", "Basavanagar", "Basavanagudi",
+            "Basaveshwaranagar", "Begur Road", "Belandur", "Bellary Road", "Benson Town", "Bilekahalli", "Bommanahalli", "Bommasandra",
+            "Brooke Field", "Btm Layout", "C V Raman Nagar", "Central Silk Board", "Chamarajpet", "Chambal River", "Chandapur", "Chikkaballapur",
+            "Chikkajala", "Cookes Town", "Cox Town", "Defence Colony", "Devanahalli", "Dodballapur Road", "Doddaballapur", "Doddaballapur Road",
+            "Dollars Colony", "Domlur", "Electronic City", "Fraser Town", "G M Palya", "Ganganagar","Gottigere", "Hanumanth Nagar",
+            "Hbr Layout", "Hebbal", "Hebbal Kempapura", "Hegde Nagar", "Hennur", "Hennur Road",
+            "Hesaraghatta Main Road", "Hmt Layout", "Hoodi Village", "Horamavu", "Hoskote", "Hoskote", "Hosur Road", "Hrbr Layout",
+            "Hsr Layout", "Hulimavu", "Huskur", "Indira Nagar", "Indraprastha", "Isro Layout", "Itpl", "Jakkur", "Jalahalli", "Jayanagar",
+            "Jeevan Bima Nagar", "Jigani Industrial Area", "Jp Nagar", "Kaggadaspura", "Kalyan Nagar", "Kanaka Nagar", "Kanakapura Road",
+            "Kasturi Nagar", "Kengeri", "Kodigehalli", "Koramangala", "Kr Puram", "Kudlu Gate", "Kumaraswamy Layout", "Kundalahalli",
+            "Lavelle Road", "Madiwala", "Magadi Road", "Mahadevapura", "Majestic", "Mallesh Palaya", "Malleshwaram", "Manek Chowk",
+            "Marathahalli", "Mathikere", "Mg Road", "Millers Road", "Mysore Road", "Naganathapura", "Nagarbhavi", "Nagawara", "Nagwar",
+            "Nandi Hills", "Nelamangala", "Old Airport Road", "Old Madras Road", "Ombr Layout", "Outer Ring Road", "Padmanabhanagar",
+            "Pai Layout", "Palace Road", "Peenya", "Prashanth Nagar", "Raj Bhavan", "Rajajinagar", "Rajanukunte", "Rajarajeshwari Nagar",
+            "Ramamurthy Nagar", "Rbi Layout", "Rest House Road", "Richards Tow", "Richmond Road", "Rmv Extension Stage", "Rt Nagar",
+            "Sadaramangala", "Sadaramangala", "Sahakar Nagar", "Sanjay Nagar", "Sarjapur", "Sarjapur Road", "Shanti Nagar", "Silkboard",
+            "Thanisandra", "Thippasandra", "Thyagaraj Nagar", "Tippasandra", "Tumkur Road", "Ulsoor", "Uttarahalli", "Vasanth Nagar",
+            "Vidyanagar", "Vidyaranyapura", "Vigyan Nagar", "Vijaya Bank Layout", "Vijayanagar", "Whitefield", "Wilson Garden", "Yelahanka",
+            "Yeshwantpur" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_pick_area);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_area, pickArea));
+
+        ListView listView = getListView();
+        listView.setTextFilterEnabled(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Intent intent = new Intent(context,MainActivity.class);
+                intent.putExtra("area", ((TextView) view).getText());
+                startActivity(intent);
+             /*   Toast.makeText(getApplicationContext(),
+                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();*/
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_pick_area, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
